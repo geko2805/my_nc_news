37cfe14b768f584fc7103cee263ec3138851b570
geko2805 <gethin.jones28@gmail.com>
2024-11-26 10:59:59 +0000

    complete task 5

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -87,3 +87,30 @@ describe("GET /api/articles/:article_id", () => {
       });
   });
 });
+
+describe("GET /api/articles", () => {
+  test("200: Responds with an array of article objects with correct properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles.length).not.toBe(0);
+        articles.forEach((article) => {
+          expect(article).not.toHaveProperty("body");
+
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("number");
+        });
+
+        expect(articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+});
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,10 +1,21 @@
-const { readArticleById } = require("../models/articles.model");
+const {
+  readAllArticles,
+  readArticleById,
+} = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   readArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getAllArticles = (req, res, next) => {
+  readAllArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
     })
     .catch(next);
 };
