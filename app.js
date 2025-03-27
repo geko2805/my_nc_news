d98876101ef52dc901e0506db26b213c8fb80bb9
geko2805 <gethin.jones28@gmail.com>
2025-03-27 18:05:40 +0000

    add endpoint for user by username

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const {
   customErrorHandler,
   serverErrorHandler,
 } = require("./error-handlers");
-const { getUsers } = require("./controllers/users.controller");
+const {
+  getUsers,
+  getUserByUsername,
+} = require("./controllers/users.controller");
 
 app.use(express.json());
 
@@ -31,6 +34,7 @@ app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getAllArticles);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 app.get("/api/users", getUsers);
+app.get("/api/users/:username", getUserByUsername);
 
 app.post("/api/articles/:article_id/comments", postComment);
 
--- models/users.model.js
+++ models/users.model.js
@@ -5,3 +5,19 @@ exports.readAllUsers = () => {
     return result.rows;
   });
 };
+
+exports.readUserByUsername = (username) => {
+  let queryString = `SELECT users.username, 
+      users.avatar_url, 
+      users.name
+        FROM users
+        WHERE users.username = $1`;
+  const queryVals = [username];
+
+  return db.query(queryString, queryVals).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "User not found" });
+    }
+    return rows[0];
+  });
+};
